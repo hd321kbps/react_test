3e2088fd87b030424435db2b22248b47c5073798
hd321kbps <hd321kbps@yandex.ru>
2018-04-23 08:38:55 +0300

    jsx style

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ import './index.css';
 const title = React.createElement('h1', null, 'React Todo');
 const subtitle = React.createElement('p', {className:'subtitle'}, 'Это мое первое приложение на React');
 const container = React.createElement('div', null, title, subtitle)
-
-ReactDOM.render(container, document.getElementById('root'));
+const app = <div>
+    <h1>React Todo</h1>
+    <p className="subtitle">Это мое первое приложение на React</p>
+</div>;
+ReactDOM.render(app, document.getElementById('root'));
