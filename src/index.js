fd7f7b286df5def94b599987cdd430744501ddf2
hd321kbps <hd321kbps@yandex.ru>
2018-04-23 08:40:07 +0300

    debug

--- src/index.js
+++ src/index.js
@@ -5,8 +5,18 @@ import './index.css';
 const title = React.createElement('h1', null, 'React Todo');
 const subtitle = React.createElement('p', {className:'subtitle'}, 'Это мое первое приложение на React');
 const container = React.createElement('div', null, title, subtitle)
-const app = <div>
-    <h1>React Todo</h1>
-    <p className="subtitle">Это мое первое приложение на React</p>
-</div>;
+// Способ через jsx
+const app = (
+    <div>
+        <h1>React Todo</h1>
+        <p className="subtitle">Это мое первое приложение на React</p>
+    </div>
+);
+console.log(title);
+console.log(subtitle);
+console.log(container);
+
+const dom = ReactDOM.render(container, document.getElementById('root'));
+console.log(dom);
 ReactDOM.render(app, document.getElementById('root'));
+console.log(app);
